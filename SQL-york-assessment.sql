03f0f5c52c7f23dc52a1e2155870ea5dd140c91f
ashoordoushow <ashoor12323@gmail.com>
2025-08-25 18:52:06 -0500

    SQL information

---
+++ SQL-york-assessment.sql
@@ -0,0 +1,32 @@
+-- Queries - in SQL a query is a command you write to ask the database to do something - usually to get, change, or manage data.
+
+Ex. 
+
+| Query Type     | What it does                        | Example                    |
+| -------------- | ----------------------------------- | -------------------------- |
+| `SELECT`       | **Get data** from a table           | `SELECT * FROM users;`     |
+| `INSERT`       | **Add data** to a table             | `INSERT INTO users (...)`  |
+| `UPDATE`       | **Change existing data** in a table | `UPDATE users SET ...`     |
+| `DELETE`       | **Remove data** from a table        | `DELETE FROM users ...`    |
+| `CREATE TABLE` | **Create** a new table              | `CREATE TABLE users (...)` |
+
+
+-- SQL is a standard language for storing, manipulating and retrieving data in databases
+-- W3schools SQL tutorial will teach me how to use SQL in: MySQL, SQL Server, MS Access, Oracle, Sybase, Informix, Postgres, and other database systems
+-- 
+
+-- What Can SQL do?
+-- SQL can execute queries against a database
+-- SQL can retrieve data from a database
+-- SQL can insert records in a database
+-- SQL can update records in a database
+-- SQL can delete records from a database
+-- SQL can create new databases (for example for a company they can have a database labeled sales, and another database labeled accounting with tables & information in each of these databases (NOT changing SQL systems) all remain in postgresSQL for example so the sales team can access their data, and maybe the accounting team wants to access their data.)
+-- SQL can create new tables in a database
+-- SQL can create stored procedures in a database (creating stored procedures is writing a sequence of SQL queries to manipulate data once. Once you've called it, now the procedure is saved in the database, and you can use it again by simply just calling it again.)
+-- SQL can create views in a database <----- THIS IS WHERE I LEFT OFF AT!!!!
+-- SQL can set permissions on tables, procedures, and views
+
+-- Scalable - means the code, app, or system can handle more users, data, or traffic without breaking or slowing down.
+
+-- Traffic - means the number of users, requests, or data moving between users and your app or website.
